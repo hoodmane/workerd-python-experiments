5f262c914b39ffdbc4e5e5823ddffa2d04ec692c
Hood Chatham <roberthoodchatham@gmail.com>
2023-10-03 09:35:17 -0700

    Add Python code handler in worker.mjs

--- src/worker.mjs
+++ src/worker.mjs
@@ -1,6 +1,5 @@
 import { loadPyodide } from "./python.mjs";
 
-// await doStuff();
 
 export default {
   async fetch(request) {
@@ -10,7 +9,21 @@ export default {
       // favicon?
       return new Response("");
     }
-    const res = await loadPyodide();
-    return new Response("xyz");
+    const t1 = performance.now();
+    const pyodide = await loadPyodide();
+    const t2 = performance.now();
+
+    pyodide.runPython(`
+      async def handle_request(req):
+          from js import fetch, Response
+          resp = await fetch("http://example.com")
+          return Response.new(await resp.arrayBuffer())
+    `);
+    const handle_request = pyodide.globals.get("handle_request");
+    const result = await handle_request(request);
+    const t3 = performance.now();
+    console.log("bootstrap", t2-t1);
+    console.log("handle", t3-t2);
+    return result;
   },
 };
