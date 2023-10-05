3a84048b2c47cd0cda751d3cb5bf0837779675fb
Hood Chatham <roberthoodchatham@gmail.com>
2023-10-05 14:36:03 -0700

    Update hooks.mjs

--- hooks.mjs
+++ hooks.mjs
@@ -1,23 +1,21 @@
 import { readFile } from 'node:fs/promises';
 
 export async function load(url, context, nextLoad) {
-  if(url.endsWith(".zip")) {
-    url = url.slice("file://".length);
-    const bin = await readFile(url);
+  const path = url.slice("file://".length);
+  if(path.endsWith(".zip") || path.endsWith(".py") || path.endsWith(".dat")) {
+    const bin = await readFile(path);
     const b64 = bin.toString('base64');
     const source = `export default Buffer.from("${b64}", "base64");`
     return {format: 'module', shortCircuit: true, source};
   }
-  if(url.endsWith(".dat")) {
-    url = url.slice("file://".length);
-    const bin = await readFile(url);
+  if(path.endsWith(".dat")) {
+    const bin = await readFile(path);
     const b64 = bin.toString('base64');
     const source = `export default undefined;`
     return {format: 'module', shortCircuit: true, source};
   }
-  if (url.endsWith(".wasm")) {
-    url = url.slice("file://".length);
-    const bin = await readFile(url);
+  if (path.endsWith(".wasm") || path.endsWith(".so")) {
+    const bin = await readFile(path);
     const b64 = bin.toString('base64');
     const source = `export default new WebAssembly.Module(Buffer.from("${b64}", "base64"));`
     return {format: 'module', shortCircuit: true, source};
